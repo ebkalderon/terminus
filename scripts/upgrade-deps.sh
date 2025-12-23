753405796e9608e629afa89d08b4208d3ec05039
Eyal Kalderon <ebkalderon@gmail.com>
2025-12-23 17:09:39 -0500

    Allow all default KaTeX delimiters, including inline $
    This commit updates the KaTeX auto-render extension to allow _all_
    default delimiters plus `$` (and not just `$$`, `$`, `\(`, and `\[`).

--- scripts/upgrade-deps.sh
+++ scripts/upgrade-deps.sh
@@ -50,9 +50,14 @@ cat <<'EOF' | tr -d '[:space:]' >> "${KATEX_MIN_JS_PATH}"
                 {left: "$$", right: "$$", display: true},
                 {left: "$", right: "$", display: false},
                 {left: "\\(", right: "\\)", display: false},
-                {left: "\\[", right: "\\]", display: true},
+                {left: "\\begin{equation}", right: "\\end{equation}", display: true},
+                {left: "\\begin{align}", right: "\\end{align}", display: true},
+                {left: "\\begin{alignat}", right: "\\end{alignat}", display: true},
+                {left: "\\begin{gather}", right: "\\end{gather}", display: true},
+                {left: "\\begin{CD}", right: "\\end{CD}", display: true},
+                {left: "\\[", right: "\\]", display: true}
             ],
-            throwOnError : false,
+            throwOnError : false
         });
     });
 EOF
